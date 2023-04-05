1259b368e4a861b64ffbed0f5c2a495bdda1682d
ZachGeo <zachageo95@gmail.com>
2023-04-05 15:05:28 +0300

    feat: Format & calculate end date of ssl

--- ssl_expiration.sh
+++ ssl_expiration.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-openssl_output=$(echo | openssl s_client -connect stackoverflow.com:443 -servername stackoverflow.com 2> /dev/null |  openssl x509 -noout -enddate)
+IFS=','
+INPUT_FILE=$1
+output=()
 
-month=$(echo $openssl_output | awk '{ print $1 }' | cut -d "=" -f2)
-day=$(echo $openssl_output | awk '{ print $2 }')
-year=$(echo $openssl_output | awk '{ print $4 }')
+while read hostname port servername
+do
+	openssl_output=$(echo | openssl s_client -connect $hostname:$port -servername $servername 2> /dev/null |  openssl x509 -noout -enddate)
 
-expiration_date=$(date -d "${month} ${day} ${year}" "+%Y-%m-%d")
+        month=$(echo $openssl_output | awk '{ print $1 }' | cut -d "=" -f2)
+        day=$(echo $openssl_output | awk '{ print $2 }')
+        year=$(echo $openssl_output | awk '{ print $4 }')
 
-echo $expiration_date
+        expiration_date=$(date -d "${month} ${day} ${year}" "+%Y-%m-%d")
+	
+        echo $servername, $expiration_date
+
+done < $INPUT_FILE
